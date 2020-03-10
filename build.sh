1f9ef3527420a02e962ca1cd5540f3aff632d6b6
Cameron Smith <smithc11@rpi.edu>
2020-03-09 22:46:12 -0400

    the evil xlomp_ser lib....

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 module load xl_r/16.1.1 spectrum-mpi
 set -x
-mpifort -qsmp hello.f90 -o fhello
-mpicc -qsmp hello.c -o chello
+mpifort -qsmp=omp hello.f90 -o fhello -L/opt/ibm/lib/ -lxlomp_ser
+#mpifort -qsmp=omp hello.f90 -o fhello -L/opt/ibm/lib/ -lxlomp_ser # spack petsc adds this
+mpicc -qsmp=omp hello.c -o chello
 set +x
